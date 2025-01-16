63be18f57e41f7fbb10d50b2ff5ed797a3b94efe
raheem <raheemideadev@gmail.com>
2025-01-16 22:02:52 +0500

    Initial;

---
+++ app/src/main/java/xyz/teamgravity/serializationcustomserializerdemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.serializationcustomserializerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.serializationcustomserializerdemo.ui.theme.SerializationCustomSerializerDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SerializationCustomSerializerDemoTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { padding ->
+
+                }
+            }
+        }
+    }
+}
